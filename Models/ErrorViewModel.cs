624433be7b5d1ab7e95251f18d56a62e5d8e5803
Neodynamic Dev Team <neodynamic@users.noreply.github.com>
2017-08-28 16:37:06 -0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebClientPrint3AspNetCore2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
